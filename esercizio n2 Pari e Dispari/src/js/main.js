d180aa46d07c907467b9e69004f02c3a43c7cce8
Soniamy <marianna.marzullo@gmail.com>
2023-12-12 16:16:03 +0100

    SCOMPOSIZIONE DEL PROBLEMA A PAROLE

---
+++ esercizio n2 Pari e Dispari/src/js/main.js
@@ -0,0 +1,6 @@
+//Pari e Dispari
+//L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//Generare un numero random (sempre da 1 a 5) per il computer
+//Sommiamo i due numeri
+//se la somma dei due numeri è pari allora vince il giocatore
+//altrimenti vince il computer
