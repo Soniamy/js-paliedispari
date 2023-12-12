85390bf3716ea4ea13a8883b83d295112c630590
Soniamy <marianna.marzullo@gmail.com>
2023-12-12 16:07:13 +0100

    fine primo esercizio

--- esercizio 1 Palidroma/src/js/main.js
+++ esercizio 1 Palidroma/src/js/main.js
@@ -6,3 +6,16 @@
 //RINCOMPORRE LA PAROLA
 //SE E' PALINDROMA DICHIARARE CHE QUESTA PAROLA E' PALINDROMA
 // ALTRIMENTI NON E' PALINDROMA
+let parola = prompt('Inserisci parola');
+function reverseString(string) {
+    let splitString = string.split('');
+    let reverseArrey = splitString.reverse();
+    let joinArray = reverseArrey.join('');
+    if (parola == joinArray) {
+        return parola + ' è palindroma';
+    } else {
+        return parola + ' non è palindroma';
+    }
+}
+reverseString(parola);
+console.log(reverseString(parola));
